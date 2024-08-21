77d298959fd76cb724ff2549c083cffaddc93fb3
ranggakrisnaa <infinity.duos@gmail.com>
2024-08-22 00:50:28 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Providers } from "@/utils/cakraProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
