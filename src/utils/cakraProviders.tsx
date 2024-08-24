6c7cc701bdc8848e57164380bc49476f447a9aa5
ranggakrisnaa <infinity.duos@gmail.com>
2024-08-24 20:47:46 +0700

    fix: testing husky

--- src/utils/cakraProviders.tsx
+++ src/utils/cakraProviders.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import theme from "@/theme/theme";
-import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider } from "@chakra-ui/react";
+import theme from '@/theme/theme';
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </CacheProvider>
-  );
+    return (
+        <CacheProvider>
+            <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        </CacheProvider>
+    );
 }
